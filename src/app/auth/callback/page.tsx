0a5c37be9f493c63421668055a39fdecbdb18e5d
vinicius ribeiro <viniciusrad@yahoo.com.br>
2025-01-18 14:59:29 -0300

    fix - ajustes para o build de produção - avaliar a retirada do authService

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { Box, CircularProgress, Typography } from '@mui/material';
-import { authService } from '@/lib/supabase/services/authService';
+//import { authService } from '@/lib/supabase/services/authService';
 
 export default function AuthCallbackPage() {
   const router = useRouter();
@@ -11,13 +11,13 @@ export default function AuthCallbackPage() {
   useEffect(() => {
     const processarCallback = async () => {
       try {
-        const session = await authService.getSession();
-        if (session) {
-          await authService.criarOuAtualizarUsuario(session);
-          router.push('/');
-        } else {
-          router.push('/login');
-        }
+        // const session = await authService.getSession();
+        // if (session) {
+        //   await authService.criarOuAtualizarUsuario(session);
+        //   router.push('/');
+        // } else {
+        //   router.push('/login');
+        // }
       } catch (error) {
         console.error('Erro no callback:', error);
         router.push('/login');
